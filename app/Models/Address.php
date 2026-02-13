5791badb8d20f6f92d4e7631e1d4fbeabf991f82
Sean Alden <sa2596491@gmail.com>
2026-02-13 23:27:18 +0700

    Add casts in address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -38,6 +38,7 @@ class Address extends Model
     ];
 
     protected $casts = [
+        'details' => 'array',
         'receiver' => 'array', // Tambahkan juga jika receiver formatnya JSON
         'is_default' => 'boolean',
     ];
