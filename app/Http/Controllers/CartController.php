66aa2eac89feb094103e33a8455c50c2d62a4f59
Sean Alden <c14210052@john.petra.ac.id>
2026-03-16 21:09:19 +0700

    code cleaning #2

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,47 +15,6 @@ class CartController extends Controller
         return response()->json($carts);
     }
 
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'product_id' => 'required|exists:products,id',
-    //         'quantity' => 'required|integer|min:1'
-    //     ]);
-
-    //     $product = Product::findOrFail($request->product_id);
-    //     $user = $request->user();
-
-    //     // Cari apakah produk sudah ada di keranjang user
-    //     $cartItem = Cart::where('user_id', $user->id)
-    //         ->where('product_id', $product->id)
-    //         ->first();
-
-    //     $newQuantity = $cartItem ? $cartItem->quantity + $request->quantity : $request->quantity;
-
-    //     // VALIDASI STOK
-    //     if ($newQuantity > $product->stock) {
-    //         return response()->json(['message' => 'Quantity exceeds available stock!'], 422);
-    //     }
-
-    //     $price = $product->discount_price ?? $product->price;
-
-    //     if ($cartItem) {
-    //         $cartItem->update([
-    //             'quantity' => $newQuantity,
-    //             'gross_amount' => $newQuantity * $price
-    //         ]);
-    //     } else {
-    //         Cart::create([
-    //             'user_id' => $user->id,
-    //             'product_id' => $product->id,
-    //             'quantity' => $request->quantity,
-    //             'gross_amount' => $request->quantity * $price
-    //         ]);
-    //     }
-
-    //     return response()->json(['message' => 'Added to cart successfully']);
-    // }
-
     public function store(Request $request)
     {
         $request->validate([
